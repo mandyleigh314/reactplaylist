c377a821b513396085618fe7ddaa9c88c3b0654c
Mandy Jacobsen <Mandy@Knightowltrivia.com>
2017-08-24 14:02:38 -0400

    everything works

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -27,8 +27,7 @@ _notes = (e) => {
   this.setState({songNotes: e.target.value})
 }
 addToList = (e) => {
-    e.preventDefault();
-    // this.setState({userName: e.target.value, songTitle: e.target.value, songArtist: e.target.value, songNotes: e.target.value});
+    e.preventDefault()
     const { userName, songTitle, songArtist, songNotes } = this.state
     let listItem = JSON.stringify({ userName, songTitle, songArtist, songNotes });
 
@@ -48,20 +47,14 @@ addToList = (e) => {
 .catch(err => {
     console.log(err, "boo!");
   });
-
+this.setState({userName: '', songArtist: '', songTitle: '', songNotes: ''})
 }
-// _submit = (e) => {
-//   e.preventDefault()
-//   const song = {userName: this.state.userName, songArtist: this.state.songArtist, songTitle: this.state.songTitle, songNotes: this.state.songNotes}
-//   this.setState({userName: '', songArtist: '', songTitle: '', songNotes: '', songs: [song, ...this.state.songs]})
-// }
 
 fetchData = (e) => {
       e.preventDefault();
       fetch('https://tiny-lasagna-server.herokuapp.com/collections/playlisting').then(results => {
         return results.json();
       }).then(data => {
-        console.log(data)
         this.setState({songs: data});
       })
     }
@@ -77,7 +70,8 @@ componentDidMount() {
 render() {
   return (  <div className="app">
     <div className="playlist">
-      <Playlistform addToList={this.addToList} _name={this._name} _artist={this._artist} _title={this._title} _notes={this._notes} _submit={this._submit}/>
+      <Playlistform addToList={this.addToList} _name={this._name}
+        userName={this.state.userName} _artist={this._artist} songArtist={this.state.songArtist} _title={this._title} songTitle={this.state.songTitle} _notes={this._notes} songNotes={this.state.songNotes} />
     </div>
   <form onSubmit={this.fetchData}>
     <input type="submit" value="Update Playlist" />
--- src/components/Playlistform.js
+++ src/components/Playlistform.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react'
 import '../styles/App.css'
 
 class Playlistform extends Component {
-  constructor(props) {
-    super(props)
-
-  }
-
 render() {
   return (
     <form onSubmit={this.props.addToList}>
